4c62d6694c1314056e28cd2a93550ad2407cac39
Yana Vasko <vask.yana@gmail.com>
2020-12-21 22:22:10 +0200

    homework 4, polymorphism

--- src/com/solvd/games/sportgames/Biathlon.java
+++ src/com/solvd/games/sportgames/Biathlon.java
@@ -1,7 +1,24 @@
 package com.solvd.games.sportgames;
 
-public class Biathlon extends WinterSportGames {
+public class Biathlon extends WinterSportGames implements Sport {
+
+    private double firstPlaceSpeed;
+
     public Biathlon() {
         super();
+    }
+    
+    public Biathlon(double firstPlaceSpeed) {
+        this.firstPlaceSpeed = firstPlaceSpeed;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("The top speed during this " + this.getClass().getSimpleName() + " competions was " + firstPlaceSpeed + " min");
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("======" + this.getClass().getSimpleName() + " is Winter Olympic Game=====");
     }
 }
--- src/com/solvd/games/sportgames/Hockey.java
+++ src/com/solvd/games/sportgames/Hockey.java
@@ -1,11 +1,24 @@
 package com.solvd.games.sportgames;
 
-public class Hockey extends WinterSportGames {
-    public Hockey(String winner) {
-        super(winner);
+public class Hockey extends WinterSportGames implements Sport{
+    
+    private String country;
+
+    public Hockey(String country) {
+        this.country = country;
     }
 
     public Hockey() {
         super();
+    }
+
+    @Override
+    public void play() {
+        System.out.println(this.getClass().getSimpleName() + " is the national game of " + country + ". A lot of people play there in such game");
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("======" + this.getClass().getSimpleName() + " is Winter Olympic Game=====");
     }
 }
