fa75216f54b748415ef5f32ec58944a784580686
Yana Vasko <vask.yana@gmail.com>
2020-12-21 22:22:10 +0200

    homework 4, polymorphism

--- src/com/solvd/games/sportgames/Canoeing.java
+++ src/com/solvd/games/sportgames/Canoeing.java
@@ -1,7 +1,7 @@
 package com.solvd.games.sportgames;
 
 public class Canoeing extends SummerSportGames {
-    public Canoeing() {
-        super();
+    public Canoeing(String winner) {
+        setWinner(winner);
     }
 }
